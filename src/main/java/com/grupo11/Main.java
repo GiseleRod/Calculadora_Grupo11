ea5ae35716631fe2ea4e8714935206f9ed6e6bc0
pereyrav031-svg <pereyrav031@gmail.com>
2026-05-15 09:54:40 -0300

    Add documentation comments to Main class
    Added documentation comments to the Main class and main method.

--- src/main/java/com/grupo11/Main.java
+++ src/main/java/com/grupo11/Main.java
@@ -1,8 +1,17 @@
 package com.grupo11;
-
+/**
+*clase principal del programa.
+*contiene el método main que inicia la ejecución de la calculadora.
+*/
 public class Main {
+/**
+*método principal que se ejecuta al iniciar el programa.
+*crea una instacia del menu y comienza la interación con el usuario.
+*/
     public static void main(String[] args) {
+        //se crea el objeto menú
         Menu menu = new Menu();
+        //se inicia el programa mostrando menú
         menu.iniciar();
         
     }
