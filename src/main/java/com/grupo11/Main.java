60db46a95730ae9d2d48f8cd1de8fb7e038b9f42
pereyrav031-svg <pereyrav031@gmail.com>
2026-05-15 13:22:24 -0300

    Update comments for clarity and consistency

--- src/main/java/com/grupo11/Main.java
+++ src/main/java/com/grupo11/Main.java
@@ -1,12 +1,14 @@
 package com.grupo11;
 /**
-*clase principal del programa.
-*contiene el método main que inicia la ejecución de la calculadora.
+* Clase principal del programa.
+* Contiene el método main que inicia la ejecución de la calculadora.
 */
 public class Main {
 /**
-*método principal que se ejecuta al iniciar el programa.
-*crea una instacia del menu y comienza la interación con el usuario.
+* Método principal que se ejecuta al iniciar el programa.
+* Crea una instancia de Menu y comienza la interacción con el usuario.
+*
+* @param args argumentos recibidos por consola.
 */
     public static void main(String[] args) {
         //se crea el objeto menú
